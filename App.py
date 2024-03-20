9751220728972121b4a7479292449bc690e89b1c
Avi Chapnick <AviCh@amdocs.com>
2024-03-20 13:57:22 +0200

    create seperate pages

--- App.py
+++ App.py
@@ -6,74 +6,81 @@ from chains.chatgpt import executeQuery
 from chains.tmfRAG import TMFRAG
 from langchain.docstore.document import Document
 
+
+
 print('******Im in the begining of the code')
 load_dotenv()
-mytmfRAG = TMFRAG()
-my3GPPRAG = TMFRAG()
-mytmfRAG.initTMF(dbPath="C:/dev/hackathon/db/TMF/")
-my3GPPRAG.initTMF(dbPath="C:/dev/hackathon/db/3GPP/")
-class Standard(Enum):
-    TMF = 'TMF'
-    THREEGPP = '3GPP'
-    CHATGPT = 'CHATGPT'
-with st.sidebar:
-    st.markdown(
-        "Please select the standard you want to use \n"
-        "1. TMF \n"
-        "2. 3GPP \n"
-        "3. use CHATGPT\n"
-    )   
-# Streamlit UI elements
-GPTAnswer = None
-ThreeGPPAnswer= None
-TMFAnswer = None
-st.title("Telco Standard Guru")
-selectedStandard = st.selectbox(
-            'Choose Standard',
-            [source.value for source in Standard]
-)
+def AssitantPage():
+    mytmfRAG = TMFRAG()
+    my3GPPRAG = TMFRAG()
+    mytmfRAG.initTMF(dbPath="C:/dev/hackathon/db/TMF/")
+    my3GPPRAG.initTMF(dbPath="C:/dev/hackathon/db/3GPP/")
+    class Standard(Enum):
+        TMF = 'TMF'
+        THREEGPP = '3GPP'
+        CHATGPT = 'CHATGPT'  
+    # Streamlit UI elements
+    GPTAnswer = None
+    ThreeGPPAnswer= None
+    TMFAnswer = None
+    st.title("Telco Standard Guru")
+    selectedStandard = st.selectbox(
+                'Choose Standard',
+                [source.value for source in Standard]
+    )
 
-question = st.text_input(
-    "Ask Somthing Our Standard guru",
-    placeholder="How should I create an TMF intent",
-    disabled=not selectedStandard
-)
+    question = st.text_input(
+        "Ask Somthing Our Standard guru",
+        placeholder="How should I create an TMF intent",
+        disabled=not selectedStandard
+    )
 
 
-if question and (selectedStandard == Standard.CHATGPT.value):
-    GPTAnswer = executeQuery(question)
-if question and (selectedStandard == Standard.TMF.value):
-    TMFAnswer = mytmfRAG.queryTMF(question)
-if question and (selectedStandard == Standard.THREEGPP.value):
-    ThreeGPPAnswer = my3GPPRAG.queryTMF(question)
+    if question and (selectedStandard == Standard.CHATGPT.value):
+        GPTAnswer = executeQuery(question)
+    if question and (selectedStandard == Standard.TMF.value):
+        TMFAnswer = mytmfRAG.queryTMF(question)
+    if question and (selectedStandard == Standard.THREEGPP.value):
+        ThreeGPPAnswer = my3GPPRAG.queryTMF(question)
+        
+    # print (f'GPTAnswer :{GPTAnswer}')
+    # print (f'ThreeGPPAnswer :{ThreeGPPAnswer}')
+    # print (f'TMFAnswer :{TMFAnswer}')
+
+    if GPTAnswer != None:
+        st.title(body="GPT Answer")
+        st.text_area(label="GPT answer",value= GPTAnswer.content,height=400)    
+    if ThreeGPPAnswer != None:
+        st.title(body="3GPP Answer")
+        st.text_area(label="3GPP answer",value= ThreeGPPAnswer['result'],height=400)
+        documents = ThreeGPPAnswer['source_documents'];
+        documentsSet = set()
+        for document in documents:
+            documentsSet.add(os.path.basename(document.metadata['source']))
+        st.title(body="3GPP Source Documents")
+        for document in documentsSet:
+            st.text(document)
+    if TMFAnswer != None:
+        st.title(body="TMF Answer")
+        st.text_area(label="TMF answer",value=TMFAnswer['result'],height=400)
+        st.title(body="TMF Source Documents")
+        documents = TMFAnswer['source_documents'];
+        documentsSet = set()
+        for document in documents:
+            documentsSet.add(os.path.basename(document.metadata['source']))
+        for document in documentsSet:
+            st.text(document)
+def uploadContent():
+    st.write('Upload your content here')  
     
-# print (f'GPTAnswer :{GPTAnswer}')
-# print (f'ThreeGPPAnswer :{ThreeGPPAnswer}')
-# print (f'TMFAnswer :{TMFAnswer}')
+page_names_to_funcs = {
+    # "—": intro,
+    "Use Telco Assitent": AssitantPage,
+    "Uploading content": uploadContent
+}
 
-if GPTAnswer != None:
-    st.title(body="GPT Answer")
-    st.text_area(label="GPT answer",value= GPTAnswer.content,height=400)    
-if ThreeGPPAnswer != None:
-    st.title(body="3GPP Answer")
-    st.text_area(label="3GPP answer",value= ThreeGPPAnswer['result'],height=400)
-    documents = ThreeGPPAnswer['source_documents'];
-    documentsSet = set()
-    for document in documents:
-        documentsSet.add(os.path.basename(document.metadata['source']))
-    st.title(body="3GPP Source Documents")
-    for document in documentsSet:
-        st.text(document)
-if TMFAnswer != None:
-    st.title(body="TMF Answer")
-    st.text_area(label="TMF answer",value=TMFAnswer['result'],height=400)
-    st.title(body="TMF Source Documents")
-    documents = TMFAnswer['source_documents'];
-    documentsSet = set()
-    for document in documents:
-        documentsSet.add(os.path.basename(document.metadata['source']))
-    for document in documentsSet:
-        st.text(document)
+demo_name = st.sidebar.selectbox("Choose a page", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
     
     
      
